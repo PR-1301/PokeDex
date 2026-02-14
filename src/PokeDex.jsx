3d6a3dfcf0790d4e7277850c91fceda90cfa7257
PR <ponnurajan.1314@gmail.com>
2026-02-14 20:11:24 +0530

    Added Loading

--- src/PokeDex.jsx
+++ src/PokeDex.jsx
@@ -1,39 +1,60 @@
 import { useEffect, useState } from "react";
 
-const PokeDex = () =>{
+const PokeDex = () => {
+  const [input, setinput] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [input ,setinput] = useState("");
-    const [searchTerm, setSearchTerm] = useState("");
-    const [pokemon, setPokemon] = useState(null);
+  const handleSearch = () => {
+    setSearchTerm(input);
+  };
 
-    const handleSearch = ()=>{
-        setSearchTerm(input);
+ useEffect(() => {
+  if (!searchTerm) return;
+
+  const fetchPoke = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+      );
+
+      const data = await res.json();
+      setPokemon(data);
+
+    } catch (err) {
+      console.error("Error fetching Pokemon");
+      setPokemon(null);
     }
 
-    useEffect(()=>{
-        if(!searchTerm) return;
+    setLoading(false);
+  };
 
-        const fetchPoke = async ()=>{
-            let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
-            let data = await res.json();
-            setPokemon(data);
-        };   
-        fetchPoke();
-    }, [searchTerm])
-
-
-    return (
+  fetchPoke();
+}, [searchTerm]);
+  return (
+    <>
+      {pokemon && (
         <>
-            {pokemon && 
-            <>
-            <img src={pokemon.sprites.front_default} width={200} height={"auto"}/>
-            </>}
-            <input 
-            type="text" value={input} placeholder="Search for pokemon" onChange={(e) => setinput(e.target.value)}/><br></br>
-            <button onClick={handleSearch}>Search</button>
+          <img
+            src={pokemon.sprites.front_default}
+            width={200}
+            height={"auto"}
+          />
         </>
-        
-    )
-}
+      )}
+      <input
+        type="text"
+        value={input}
+        placeholder="Search for pokemon"
+        onChange={(e) => setinput(e.target.value)}
+      />
+      <br></br>
+      <button onClick={handleSearch}>Search</button>
+    </>
+  );
+};
 
 export default PokeDex;
